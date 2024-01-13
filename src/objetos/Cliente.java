d772dd9e130d3638cb5a9cb14c4fc4dfa898c64a
Rafael Vieira <rafael.vieira@vrsoft.com.br>
2024-01-13 15:45:53 -0300

    SEGUNDO COMMIT
    - Criacao das Entidades.

---
+++ src/objetos/Cliente.java
@@ -0,0 +1,16 @@
+package objetos;
+
+import java.time.LocalDate;
+
+public class Cliente {
+    public int id;
+    public String nome;
+    public String rg;
+    public String cpf;
+    public String logradouro;
+    public int id_municipio;
+    public int id_estado;
+    public String cnh;
+    public LocalDate dataVencimentoCnh;
+    public String email;
+}
---
+++ src/objetos/Seguradora.java
@@ -0,0 +1,11 @@
+package objetos;
+
+public class Seguradora {
+    public int id;
+    public String nome;
+    public String cnpj;
+    public String email;
+    public Double valor;
+    public int id_municipio;
+    public int id_estado;
+}
