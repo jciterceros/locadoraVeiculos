21239736a0ba299f3a3e5e4ba0b5a9cc6d7fdc9c
jciterceros <jciterceros@yahoo.com>
2024-01-20 09:54:56 -0300

    replace fields

--- src/objetos/Seguradora.java
+++ src/objetos/Seguradora.java
@@ -1,11 +1,103 @@
 package objetos;
 
 public class Seguradora {
-    public int id;
-    public String nome;
-    public String cnpj;
-    public String email;
-    public Double valor;
-    public int id_municipio;
-    public int id_estado;
+    private int id;
+    private String nome;
+    private String cnpj;
+    private String email;
+    private Double valor;
+    private int id_municipio;
+    private int id_estado;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the valor
+     */
+    public Double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the id_municipio
+     */
+    public int getId_municipio() {
+        return id_municipio;
+    }
+
+    /**
+     * @param id_municipio the id_municipio to set
+     */
+    public void setId_municipio(int id_municipio) {
+        this.id_municipio = id_municipio;
+    }
+
+    /**
+     * @return the id_estado
+     */
+    public int getId_estado() {
+        return id_estado;
+    }
+
+    /**
+     * @param id_estado the id_estado to set
+     */
+    public void setId_estado(int id_estado) {
+        this.id_estado = id_estado;
+    }
+    
 }
